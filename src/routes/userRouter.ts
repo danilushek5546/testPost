ca1d1570bd840c9b7e8d166dc2722f567e6e764f
danil <dan341616@gmail.com>
2022-08-30 14:47:49 +0300

    added validation and hash

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express';
 import usercontrollers from '../controllers/usercontrollers/index';
+import validatitonMiddleware from '../middlewares/validateMiddleware';
+import userSchema from '../validateSchemas/singInSchema';
 
 const router = Router();
 
 router.get('/', usercontrollers.getAllUsers);
 router.get('/:id', usercontrollers.getOne);
 router.delete('/:id', usercontrollers.deleteUser);
-router.post('/', usercontrollers.postUser);
-router.put('/:id', usercontrollers.putUser);
+router.post('/', validatitonMiddleware(userSchema), usercontrollers.postUser);
+router.put('/:id', validatitonMiddleware(userSchema), usercontrollers.putUser);
 
 export default router;
